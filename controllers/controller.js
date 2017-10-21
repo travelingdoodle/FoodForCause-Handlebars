6c94d151bceebfe04845d985a6317414c4b624d1
travelingdoodle <reggiebrown@gmail.com>
2017-10-21 16:26:57 -0500

    file structure and public assets

---
+++ controllers/controller.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var db = require ('../models');
+
+
+/*==================================EXPRESS ROUTES====================================*/
+
+router.get('/'), function(req, res){
+    // retrieve all data from food_db
+    
+
+}
---
+++ models/item.js
@@ -0,0 +1,32 @@
+/*==================================ITEM MODEL====================================*/
+
+module.exports = function(sequelize, DataTypes){
+    var Item = sequelize.define('item', {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      category: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      quantity: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      expiration: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      reserved: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: 0,
+      },
+      date: {
+        type: DataTypes.DATE,
+        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+      Item.sync();
+      return Item;
+    };
